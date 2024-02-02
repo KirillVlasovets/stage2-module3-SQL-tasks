fd1455359f9042b7544d562a3847f46cdd350595
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-02 21:58:41 +0400

    Added 1 lesson solution

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,39 @@
+create table IF NOT EXISTS Student
+(
+    id bigint primary key,
+    name varchar,
+    birthday date,
+    "group" int
+);
 
+create table if not exists Subject
+(
+    id bigint primary key,
+    name varchar,
+    description varchar,
+    grade int
+);
+
+create table if not exists PaymentType
+(
+    id bigint primary key,
+    name varchar
+);
+
+create table if not exists Payment
+(
+    id bigint primary key,
+    type_id bigint references PaymentType(id),
+    amount decimal,
+    student_id bigint references Student(id),
+    payment_date datetime
+);
+
+create table if not exists Mark
+(
+    id bigint primary key,
+    student_id bigint references Student(id),
+    subject_id bigint references Subject(id),
+    mark int
+);
 
