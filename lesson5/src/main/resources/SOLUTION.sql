cd9cfcde37b81bef3a249eca95eb9b8c7a5752ce
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-02 22:00:34 +0400

    Added 5 lesson solution

---
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -0,0 +1,20 @@
+select * from payment
+where amount >= 500;
+
+select * from student
+where birthday >= dateadd(year, -20, '2024-02-02');
+
+select * from student
+where groupnumber = 10 and birthday < dateadd(year, -20, '2024-02-02');
+
+select * from student
+where name = 'Mike' or groupnumber = 4 or groupnumber = 5 or groupnumber = 6;
+
+select * from payment
+where payment_date > dateadd(month, -8, '2024-02-02');
+
+select * from student
+where name like 'A%';
+
+select * from student
+where (name = 'Roxi' and groupnumber = 4) or (name = 'Tallie' and groupnumber = 9);
