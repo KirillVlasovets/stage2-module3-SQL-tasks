f27614fbcfa18aad49e15da40c729d22193dc201
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-03 02:57:44 +0400

    Added 11 lesson solution

---
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -0,0 +1,15 @@
+update subject
+set grade = 5
+where name = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+
+update student
+set groupnumber = 8
+where name = 'Tremaine Worvill';
+
+update payment
+set amount = 500 and student_id = 2
+where payment_date > 2021-01-01 and type_id = 2;
+
+update mark m
+set m.mark = 2
+where subject_id = 315;
