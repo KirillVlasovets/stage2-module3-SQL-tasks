47b0cb792cf9cc8b94728cab585198e53d062914
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-03 17:02:42 +0400

    Fixed 5 lesson solution

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -2,16 +2,16 @@ select * from payment
 where amount >= 500;
 
 select * from student
-where birthday >= dateadd(year, -20, '2024-02-02');
+where birthday >= dateadd(year, -20, '2024-02-03');
 
 select * from student
-where groupnumber = 10 and birthday < dateadd(year, -20, '2024-02-02');
+where groupnumber = 10 and birthday < dateadd(year, -20, '2024-02-03');
 
 select * from student
 where name = 'Mike' or groupnumber = 4 or groupnumber = 5 or groupnumber = 6;
 
 select * from payment
-where payment_date > dateadd(month, -8, '2024-02-02');
+where payment_date > dateadd(month, -8, '2024-02-03');
 
 select * from student
 where name like 'A%';
