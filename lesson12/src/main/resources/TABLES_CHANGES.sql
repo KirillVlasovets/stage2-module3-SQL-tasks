8413d8c20b2cd095e0272d9897a53d6ef352f75d
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-02-03 21:36:31 +0400

    Fixes added

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,11 +1,11 @@
-ALTER table mark drop foreign key mark_student_id_fkey;
+ALTER table mark drop constraint mark_student_id_fkey;
 
-alter table mark add foreign key (student_id) references student(id) on delete cascade;
+alter table mark add constraint mark_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
 
-alter table payment drop foreign key payment_student_id_fkey;
+alter table payment drop constraint payment_student_id_fkey;
 
-alter table payment add foreign key (student_id) references student(id) on delete cascade;
+alter table payment add constraint payment_student_id_fkey foreign key (student_id) references student(id) on delete cascade;
 
-alter table payment drop foreign key payment_type_id_fkey;
+alter table payment drop constraint payment_type_id_fkey;
 
-alter table payment add foreign key (type_id) references paymenttype(id) on delete cascade;
+alter table payment add constraint payment_type_id_fkey foreign key (type_id) references paymenttype(id) on delete cascade;
